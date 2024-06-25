9634bd624d589f5e19cec6e9e4d1b68be464a56f
MAXII\maxii <maximaxmaxii690@gmail.com>
2024-06-25 13:45:38 -0300

    login interface created

--- fronten-tp-final/src/app/app.routes.ts
+++ fronten-tp-final/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path : 'login', component: LoginComponent}
+    // { path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) },
+    // { path: 'register', loadChildren: () => import('./register/register.module').then(m => m.RegisterModule) },
+    // { path: '**', redirectTo: 'home', pathMatch: 'full' },
+];
