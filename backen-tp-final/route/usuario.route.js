51d177a2beea987ff4232da885314a5d0deb3ea6
MAXII\maxii <maximaxmaxii690@gmail.com>
2024-06-29 21:25:28 -0300

    test

--- backen-tp-final/route/usuario.route.js
+++ backen-tp-final/route/usuario.route.js
@@ -6,7 +6,7 @@ const authCtrl = require('../controller/auth.controller');
 router.get('/usuario/registro', authCtrl.registerUser);
 router.post('/usuario/login', authCtrl.loginUser);
 
-router.get('/usuarios', authCtrl.verifyToken, usuarioCtrl.getAllUsuarios);
+router.get('/usuarios', authCtrl.verifyToken, usuarioCtrl.getUsuarios);
 router.post('/usuario', authCtrl.verifyToken, usuarioCtrl.createUsuario);
 router.get('/usuario/:id', authCtrl.verifyToken, usuarioCtrl.getUsuarioById);
 router.put('/usuario/:id', authCtrl.verifyToken, usuarioCtrl.updateUsuario);
--- fronten-tp-final/src/app/components/usuario-form/usuario-form.component.ts
+++ fronten-tp-final/src/app/components/usuario-form/usuario-form.component.ts
@@ -1,55 +1,58 @@
 import { Component, Inject, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { UsuarioService } from '../../services/usuario.service';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Usuario } from '../../models/usuario';
+import { Rol } from '../../models/rol';
 
 @Component({
   selector: 'app-usuario-form',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, ReactiveFormsModule],
   templateUrl: './usuario-form.component.html',
   styleUrl: './usuario-form.component.css'
 })
-export class UsuarioFormComponent {
+export class UsuarioFormComponent implements OnInit{
+ 
 
-  // usuarioForm: FormGroup;
-  // // usuario: Usuario = new Usuario();
+  usuarioForm: FormGroup;
+  usuario: Usuario = new Usuario('', '', '', new Rol('', ''));
 
-  // private readonly _usuarioService = Inject(UsuarioService);
-  // private readonly _router = Inject(Router);
-  // private readonly _route = Inject(ActivatedRoute);
+  private readonly _usuarioService = Inject(UsuarioService);
+  private readonly _router = Inject(Router);
+  private readonly _route = Inject(ActivatedRoute);
 
-  // constructor(
-  //   private fb: FormBuilder
-  // ) {
-  //   this.usuarioForm = this.fb.group({
-  //     nombre: ['', Validators.required],
-  //     email: ['', [Validators.required, Validators.email]],
-  //     rol: ['', Validators.required]
-  //   });
-  // }
+  constructor(
+    private fb: FormBuilder
+  ) {
+    this.usuarioForm = this.fb.group({
+      nombreUsuario: ['', Validators.required],
+      password: ['', [Validators.required, Validators.minLength(5)]],
+      rol: ['', Validators.required]
+    });
+  }
 
-  // ngOnInit(): void {
-  //   const id = this._route.snapshot.paramMap.get('id');
-  //   if (id) {
-  //     this._usuarioService.getUsuario(id).subscribe(data => {
-  //       this.usuario = data;
-  //       this.usuarioForm.patchValue(this.usuario);
-  //     });
-  //   }
-  // }
-
-  // onSubmit(): void {
-  //   if (this.usuarioForm.valid) {
-  //     if (this.usuario.id) {
-  //       this._usuarioService.updateUsuario(this.usuario.id, this.usuarioForm.value).subscribe(() => {
-  //         this._router.navigate(['/usuario-crud']);
-  //       });
-  //     } else {
-  //       this._usuarioService.createUsuario(this.usuarioForm.value).subscribe(() => {
-  //         this._router.navigate(['/usuario-crud']);
-  //       });
-  //     }
-  //   }
-  // }
+  ngOnInit(): void {
+    const id = this._route.snapshot.paramMap.get('id');
+    if (id) {
+      this._usuarioService.getUsuario(id).subscribe((data: Usuario) => {
+        this.usuario = data;
+        this.usuarioForm.patchValue(this.usuario);
+      });
+    }
+  }
+  
+  onSubmit(): void {
+    if (this.usuarioForm.valid) {
+      if (this.usuario.id) {
+        this._usuarioService.updateUsuario(this.usuario.id, this.usuarioForm.value).subscribe(() => {
+          this._router.navigate(['/usuario-crud']);
+        });
+      } else {
+        this._usuarioService.createUsuario(this.usuarioForm.value).subscribe(() => {
+          this._router.navigate(['/usuario-crud']);
+        });
+      }
+    }
+  }
 }
