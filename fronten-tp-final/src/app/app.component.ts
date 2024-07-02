21b47b9b56b8d757d9d6d06d8539988df3dbf844
Nahuel277 <lautaromoyata.lm@gmail.com>
2024-07-02 01:00:26 -0300

    Se ajustó la barra de navegación y el footer

--- fronten-tp-final/src/app/app.component.ts
+++ fronten-tp-final/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
+import { NavBarComponent } from './components/shared/nav-bar/nav-bar.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [RouterOutlet, NavBarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
