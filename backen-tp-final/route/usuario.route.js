d892978500c81d7a76f210bdd345d0396c4e480d
Nahuel277 <lautaromoyata.lm@gmail.com>
2024-07-05 17:13:02 -0300

    usuario.route.js

---
+++ backen-tp-final/route/usuario.route.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+const usuarioCtrl = require('../controller/usuario.controller');
+const authCtrl = require('../controller/auth.controller');
+
+router.post('/registro', authCtrl.registerUser);
+router.post('/login', authCtrl.loginUser);
+
+router.get('/usuarios', usuarioCtrl.getAllUsuarios);
+router.post('/', usuarioCtrl.createUsuario);
+router.get('/:id', usuarioCtrl.getUsuarioById);
+router.put('/:id', usuarioCtrl.updateUsuario);
+router.delete('/:id', usuarioCtrl.deleteUsuario);
+
+module.exports = router;
