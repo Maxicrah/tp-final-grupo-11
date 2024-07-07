77a67ee2669b2c0798307cfd10422511e1b21055
Nahuel277 <lautaromoyata.lm@gmail.com>
2024-07-07 17:33:10 -0300

    Actualización del sistema de autenticación y registro
    - Modificación del backend para la creación de propietarios:
      - Actualización del controlador y las rutas de usuarios para manejar el registro de propietarios.
      - Implementación de las validaciones necesarias para los campos de registro.
    
    - Modificación del frontend:
      - Reestructuración del componente de login y registro para ajustarse a la nueva estructura.
      - Ajuste de las rutas de navegación después del registro exitoso.
    
    Estos cambios mejoran la gestión de roles y permiten el registro adecuado de propietarios en la aplicación.

--- fronten-tp-final/src/app/app.routes.ts
+++ fronten-tp-final/src/app/app.routes.ts
@@ -1,14 +1,16 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from './components/login/login.component';
 import { AdminDashboardComponent } from './components/admin-dashboard/admin-dashboard.component';
 import { HomeComponent } from './components/home/home.component';
 import { FormAlquilerComponent } from './components/alquiler/form-alquiler/form-alquiler.component';
 import { AuthGuard } from './guards/auth.guard';
+import { LoginComponent } from './website/auth/login/login.component';
+import { RegisterComponent } from './website/auth/register/register.component';
 export const routes: Routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'home', component: HomeComponent},
     { path: 'admin-dashboard', component: AdminDashboardComponent, canActivate: [AuthGuard]},
     { path : 'login', component: LoginComponent},
+    { path: 'registro', component: RegisterComponent},
     // { path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) },
     // { path: 'register', loadChildren: () => import('./register/register.module').then(m => m.RegisterModule) },
     // { path: '**', redirectTo: 'home', pathMatch: 'full' },
